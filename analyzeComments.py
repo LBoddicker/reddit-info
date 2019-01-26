8a2667bd2f9789b685a39a9ed30fbb6590999c56
Lake Boddicker <lakeboddicker@gmail.com>
2019-01-25 19:53:25 -0500

    add analyze comments

---
+++ analyzeComments.py
@@ -0,0 +1,21 @@
+# We should return whether or not the comment is positive or negative
+# We should return reading level
+# We should maintain rankings of most/least positive comments - same with reading level
+
+from textblob import TextBlob
+import json
+
+def returnSentiment(inStr):
+    myTxt = TextBlob(inStr)
+    return myTxt.sentiment.polarity
+
+def getTotSentiment(inSub):
+    totNum = 0
+    totSentiment = 0
+    with open('{0}Parsed.json'.format(inSub)) as readFile:
+        d = json.load(readFile)
+        for k, v in d.items():
+            for i in v:
+                totNum += 1
+                totSentiment += returnSentiment(i)
+    return totSentiment/totNum
