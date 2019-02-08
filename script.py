9fb01ba20608aa40fc5d1d822e53a34ac0a46c54
Lake Boddicker <lakeboddicker@gmail.com>
2019-02-07 19:57:45 -0500

    update to main script

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
 import os
 import json
-#import sys
+import commentFetch
+import RedditSQL
+import praw
+from commentParse import ParseComments
+from analyzeComments import *
+import sys
 
 
 def setupConfig():
@@ -10,7 +15,7 @@ def setupConfig():
         print('it does exist')
         with open('redditConfig.json') as of:
             myConfigDict = json.load(of)
-        
+
     else:
         keys = ['user_agent', 'client_id', 
                 'client_secret', 'username',
@@ -26,5 +31,31 @@ def setupConfig():
 
     return myConfigDict
 
-if __name__ == '__main__':
+def main():
     tempDict = setupConfig()
+    redditInstance = praw.Reddit(client_id=tempDict['client_id'],
+                                   client_secret=tempDict['client_secret'],
+                                   password=tempDict['password'],
+                                   user_agent=tempDict['user_agent'],
+                                   username=tempDict['username'])
+    sqlConnection = RedditSQL.RedditSQL('myDB')
+    myInst = commentFetch.InfoFetch(redditInstance, sqlConnection)
+    listOfSubs = ['announcements', 'funny', 'AskReddit', 'todayilearned', 'science', 'worldnews', 'pics', 'IAmA', 'gaming', 'videos',
+                  'movies', 'aww', 'Music', 'blog','gifs','news','explainlikeimfive','askscience','EarthPorn','books',
+                  'television','mildlyinteresting','LifeProTips','Showerthoughts','space','DIY','Jokes','gadgets','nottheonion','sports',
+                  'tifu','food','photoshopbattles','Documentaries','Futurology','history','InternetIsBeautiful','dataisbeautiful','UpliftingNews','listentothis',
+                  'GetMotivated','personalfinance','OldSchoolCool','philosophy','Art','nosleep','WritingPrompts','creepy','TwoXChromosomes','Fitness',
+                  'technology','WTF','bestof','AdviceAnimals','politics','athesim','interestingasfuck','europe','woahdude','BlackPeopleTwitter',
+                  'oddlysatisfying','gonewild','leagueoflegends','pcmasterrace','reactiongifs','gameofthrones','wholesomememes','Unexpected','Overwatch','facepalm',
+                  'trees','Android','lifehacks','me_irl','relationships','Games','nba','programming','tattoos','NatureIsFuckingLit',
+                  'Whatcouldgowrong','CrappyDesign','Dankmemes','nsfw','cringepics','4chan','soccer','comics','sex','pokemon',
+                  'malefashionadvice','NSFW_GIF','StarWars','Frugal','HistoryPorn','AnimalsBeingJerks','RealGirls','travel','buildapc','OutOfTheLoop']
+    tempList = []
+    for i in listOfSubs:
+        myInst.getCommentsFromSubreddit(i, submissionLimit=100)
+    sqlConnection.getSubredditTable()
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    main()
